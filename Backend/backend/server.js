eed954861290ab4512a85b83fa2db8f97858d830
Rivethika <nivethanivi849@gmail.com>
2024-03-27 16:07:58 +0530

    first commit

--- Backend/backend/server.js
+++ Backend/backend/server.js
@@ -42,7 +42,7 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ['http://localhost:3000'],
+    origin: [process.env.CLIENT_URL],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
   })
